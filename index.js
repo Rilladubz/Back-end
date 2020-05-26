36296464ee201f803c79339fc3f19dd49ce97723
Mel Maki <melody.rackham@gmail.com>
2020-05-25 19:35:43 -0600

    Server is live

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const server = require("./api/server");
 
 //define port
-const port = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
 
 server.listen(PORT, () => {
