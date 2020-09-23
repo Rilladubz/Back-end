0583a7168c57f7bcea44e8b50cda052983cbb7d9
Samuel Torres <42902403+Rilladubz@users.noreply.github.com>
2020-09-23 10:42:05 -0400

    changed

--- knexfile.js
+++ knexfile.js
@@ -4,32 +4,32 @@ module.exports = {
     useNullAsDefault: true,
     connection: {
       /* this is where we define our database name */
-      filename: "./data/todo.db3"
+      filename: "./data/todo.db3",
     },
     migrations: {
-      directory: "./data/migrations"
+      directory: "./data/migrations",
     },
     seeds: {
-      directory: "./data/seeds"
+      directory: "./data/seeds",
     },
     pool: {
       afterCreate: (conn, done) => {
         conn.run("PRAGMA foreign_keys = ON", done);
-      }
-    }
+      },
+    },
   },
   testing: {
     client: "sqlite3",
     connection: {
-      filename: "./data/test.db3"
+      filename: "./data/test.db3",
     },
     useNullAsDefault: true,
     migrations: {
-      directory: "./data/migrations"
+      directory: "./data/migrations",
     },
     seeds: {
-      directory: "./data/seeds"
-    }
+      directory: "./data/seeds",
+    },
   },
 
   production: {
@@ -38,14 +38,9 @@ module.exports = {
 
     migrations: {
       directory: "./data/migrations",
-      tableName: "knex_migrations"
     },
     seeds: {
-      directory: "./data/seeds"
+      directory: "./data/seeds",
     },
-    pool: {
-      min: 2,
-      max: 10
-    }
-  }
+  },
 };
